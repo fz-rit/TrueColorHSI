faea21ce67ffb117f6f7388d2f0380ac1366261e
Fei Zhang <fzhcis@rit.edu>
2025-02-08 17:05:25 -0500

    update version number to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TrueColorHSI',  
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     include_package_data=True,  # Ensure package data is included
     install_requires=[
